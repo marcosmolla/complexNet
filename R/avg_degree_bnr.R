791422f498ea02f305eca35e3b60ee7f8cb7f0cc
Marco Smolla <ms2070@me.com>
2021-04-30 15:29:32 -0400

    Fixed typo, brn to bnr

--- R/avg_degree_bnr.R
+++ R/avg_degree_bnr.R
@@ -1,7 +1,7 @@
 #' @title
-#' Expected average degree of BRN networks
+#' Expected average degree of BNR networks
 #' @description
-#' Calculates the expected average degree of a BRN network (single parent only) based on the approximation by Ilany and Akcay, 2016 (see details).
+#' Calculates the expected average degree of a BNR network (single parent only) based on the approximation by Ilany and Akcay, 2016 (see details).
 #'
 #' @param n Number of nodes in the network
 #' @param pb Probability to connect to parent (default is 1)
@@ -9,19 +9,19 @@
 #' @param pr Probability to connect to individuals that are not connected to
 #' @examples
 #' # Expected degree
-#' avg_degree_brn(n = 100, pb = 1, pn = .2, pr = .02)
+#' avg_degree_bnr(n = 100, pb = 1, pn = .2, pr = .02)
 #' # Compare to simulated network with identical parameters
 #' adjm <- make_bnr(n = 100, np = c(0,0), pb = 1, pn = .2, pr = .02)
 #' mean(adjm) * 100
 #' @details The expected average degree \eqn{\bar{d}} is calculated as \deqn{ \bar{d} = \frac{(N-1)(p_b + (N-2)p_r)}{N-1-(N-2)(p_n - p_r)} }
-#' @return Returns the expected average degree of a BRN network as a numeric value. This value is an analytic result and not a numeric approximation (compare examples below).
+#' @return Returns the expected average degree of a BNR network as a numeric value. This value is an analytic result and not a numeric approximation (compare examples below).
 #' @references Ilany, A., and Akçay, E. (2016). Personality and Social Networks: A Generative Model Approach. Integrative and Comparative Biology, 56(6), 1197–1205. \doi{10.1093/icb/icw068}
-#' @rdname avg_degree_brn
+#' @rdname avg_degree_bnr
 #' @export
-setGeneric("avg_degree_brn", function(n, pb, pn, pr) {standardGeneric("avg_degree_brn")})
+setGeneric("avg_degree_bnr", function(n, pb, pn, pr) {standardGeneric("avg_degree_bnr")})
 
-#' @rdname avg_degree_brn
-setMethod(f="avg_degree_brn",
+#' @rdname avg_degree_bnr
+setMethod(f="avg_degree_bnr",
           signature=c(n = "numeric",
                       pb = "numeric",
                       pn = "numeric",
